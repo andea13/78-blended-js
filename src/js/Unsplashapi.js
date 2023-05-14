6fadf5b6da2a976bee385c08fef54f3f814556c0
starik061 <starik061@gmail.com>
2023-05-14 13:16:21 +0300

    add pagination

--- src/js/Unsplashapi.js
+++ src/js/Unsplashapi.js
@@ -3,10 +3,27 @@ import axios from 'axios';
 export class Unsplashapi {
   #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
   #BASE_URL = 'https://api.unsplash.com/search/photos';
-  async getPopularImages() {
+  #query = '';
+
+  set searchQuery(newQuery) {
+    this.#query = newQuery;
+  }
+
+  async getPopularImages(page) {
     const responce = await axios.get(
-      `${this.#BASE_URL}?client_id=${this.#API_KEY}&query=popular&per_page=7`
+      `${this.#BASE_URL}?client_id=${
+        this.#API_KEY
+      }&query=popular&per_page=9&page=${page}`
     );
-    return responce;
+    return responce.data;
+  }
+
+  async getImagesByQuery(page) {
+    const responce = await axios.get(
+      `${this.#BASE_URL}?client_id=${this.#API_KEY}&query=${
+        this.#query
+      }&per_page=9&page=${page}`
+    );
+    return responce.data;
   }
 }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,10 +1,110 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
+
 import { Unsplashapi } from './Unsplashapi';
 import { createGalleryCard } from './create-gallerycard';
-const galleryList = document.querySelector('.js-gallery');
 
+const paginationElement = document.querySelector('#tui-pagination-container');
+
+const options = {
+  totalItems: 10,
+  itemsPerPage: 9,
+  visiblePages: 3,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+
+const pagination = new Pagination(paginationElement, options);
+const pageForPagination = pagination.getCurrentPage();
+
+const galleryList = document.querySelector('.js-gallery');
+const formElement = document.querySelector('.js-search-form');
 const services = new Unsplashapi();
-const respons = services.getPopularImages();
-respons.then(res => {
-  const responseCurrent = createGalleryCard(res.data.results);
-  galleryList.insertAdjacentHTML("beforeend", responseCurrent )
-});
+
+const respons = services.getPopularImages(pageForPagination);
+
+respons
+  .then(({ total, results }) => {
+    const responseCurrent = createGalleryCard(results);
+    pagination.reset(total);
+    galleryList.insertAdjacentHTML('beforeend', responseCurrent);
+  })
+  .catch(error => {
+    alert(error.message);
+  });
+
+pagination.on('afterMove', getByPopularQuery);
+
+formElement.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  pagination.off('afterMove', getByPopularQuery);
+
+  galleryList.innerHTML = '';
+  let { query } = event.target.elements;
+  query = query.value.trim();
+
+  pagination.off('afterMove', getByQuery);
+
+  services.searchQuery = query;
+  services
+    .getImagesByQuery(pageForPagination)
+    .then(({ total, results }) => {
+      pagination.reset(total);
+
+      const responseCurrent = createGalleryCard(results);
+      galleryList.innerHTML = responseCurrent;
+
+      pagination.on('afterMove', getByQuery);
+    })
+    .catch(error => {
+      alert(error.message);
+    });
+}
+
+function getByQuery(event) {
+  const currentPage = event.page;
+  services
+    .getImagesByQuery(currentPage)
+    .then(({ results }) => {
+      const responseCurrent = createGalleryCard(results);
+      galleryList.innerHTML = responseCurrent;
+    })
+    .catch(error => {
+      alert(error.message);
+    });
+}
+
+function getByPopularQuery(event) {
+  const currentPage = event.page;
+  services
+    .getPopularImages(currentPage)
+    .then(({ results }) => {
+      const responseCurrent = createGalleryCard(results);
+      galleryList.innerHTML = responseCurrent;
+    })
+    .catch(error => {
+      alert(error.message);
+    });
+}
